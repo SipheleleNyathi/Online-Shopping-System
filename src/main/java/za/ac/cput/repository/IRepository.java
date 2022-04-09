854cf31d8e3abe5f4524105025bdbff20a050061
Edvalter Jamba <edvalterjamba9@gmail.com>
2022-04-10 00:37:35 +0200

    Edvalter_Jamba_Order_&_OrderDetails_Repository and tests_Classes

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -5,9 +5,12 @@ Repository interface
 Date: 08 / 04 / 2022
  */
 
-public interface IRepository<T, ID> {
+public interface IRepository <T, ID>
+{
     T create(T t);
     T update(T t);
     T read(ID id);
     boolean delete(ID id);
+
+
 }
